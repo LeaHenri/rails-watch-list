cdb9f3e28134bed5263955cdf4299f88b4b9787a
Lea Henri <lea-henri@live.fr>
2025-03-06 21:06:38 +0100

    watch list

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -6,7 +6,7 @@ class BookmarksController < ApplicationController
 
   def create
     @list = List.find(params[:list_id])
-    @bookmark = Bookmark.new(movie_params)
+    @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
     @bookmark.save
     redirect_to list_path(@list)
@@ -19,7 +19,7 @@ class BookmarksController < ApplicationController
   end
 
   private
-  
+
   def movie_params
     params.require(:bookmark).permit(:comment)
   end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,7 +6,7 @@ class ListsController < ApplicationController
 
   def show
     @list = List.find(params[:id])
-    # @bookmarks = Bookmark.all(@list)
+    @bookmark = Bookmark.new
   end
 
   def new
