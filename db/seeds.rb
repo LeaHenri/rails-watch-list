7533cbf5a43c273ee5c9378cc54d633c31a3db76
Lea Henri <lea-henri@live.fr>
2025-03-06 14:45:26 +0100

    models & seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,28 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'json'
+require 'open-uri'
+
+puts 'Cleaning database...'
+Movie.destroy_all
+
+puts 'Creating movie seeds'
+
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+list = JSON.parse(URI.parse(url).read) # mettre du JSON en RUBY
+
+movies_list = list['results']
+
+10.times do |i|
+  hash = movies_list[i]
+  movie = Movie.new(
+    title: hash['title'],
+    overview: hash['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500/#{hash['poster_path']})",
+    rating: hash['vote_average']
+  )
+  movie.save
+end
+
+puts "Finished! Created #{Movie.count} movies."
